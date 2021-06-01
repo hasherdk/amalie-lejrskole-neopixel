0b0650c86db0ebe6543305990a3472970753851a
hasherdk <hasher@hasher.dk>
2021-06-01 17:00:12 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,64 @@
+input.onButtonPressed(Button.A, function () {
+    desiredState += 1
+    if (desiredState > 7) {
+        desiredState = 1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    brightness = 255
+    strip.setBrightness(brightness)
+    changeState(activeState)
+})
+input.onButtonPressed(Button.B, function () {
+    brightness = brightness / 2
+    strip.setBrightness(brightness)
+    changeState(activeState)
+})
+function testSequence () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    for (let index = 0; index < strip.length(); index++) {
+        basic.pause(5)
+        strip.shift(1)
+        strip.show()
+    }
+    strip.clear()
+    strip.show()
+}
+function changeState (state: number) {
+    if (state == 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (state == 2) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (state == 3) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (state == 4) {
+        strip.showRainbow(1, 360)
+    } else if (state == 5) {
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+    } else if (state == 6) {
+        strip.showColor(neopixel.rgb(230, 80, 0))
+    } else {
+        strip.clear()
+        strip.show()
+    }
+}
+let activeState = 0
+let brightness = 0
+let desiredState = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 150, NeoPixelMode.RGB)
+desiredState = 0
+brightness = 255
+testSequence()
+basic.forever(function () {
+    if (activeState != desiredState) {
+        changeState(desiredState)
+        activeState = desiredState
+    } else if (activeState == 4) {
+        strip.rotate(1)
+        strip.show()
+    } else {
+    	
+    }
+})
